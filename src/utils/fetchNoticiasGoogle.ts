ebf2c20ca2486952a6f48257f2f949ebb9d7aa9c
Jeikeiis <jeikeiis16@gmail.com>
2025-06-17 09:11:49 -0300

    Checkpoint 9.8

--- src/utils/fetchNoticiasGoogle.ts
+++ src/utils/fetchNoticiasGoogle.ts
@@ -1,4 +1,4 @@
-const API_KEY = "pub_b99564d2bb0e4b77b00b36d54202807d"; // API key pública de NewsData.io
+const API_KEY = process.env.API_USER_KEY || ""; // Ahora toma la API key de la variable de entorno
 
 export async function fetchNoticiasNewsData(region: string) {
   // Usa el endpoint /latest y la nueva API key
